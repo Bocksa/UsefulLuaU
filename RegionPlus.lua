96cdf18be773c090bc247d0345ae46d03c9baa19
benjysugi <benjysugi01@outlook.com>
2023-06-12 00:59:44 +0100

    added some extra features and documentation comments

--- RegionPlus.lua
+++ RegionPlus.lua
@@ -1,64 +1,102 @@
--- | Variables
+-- RegionPlus.lua
+
+-- // Class Definition \\
 local Region = {
 	CheckPlayerEntry = false,
 	PlayersInRegion = {},
 	PlayerEnteredRegion = script.PlayerEnteredRegion, -- Bindable Event named PlayerEnteredRegion
 	PlayerExitedRegion = script.PlayerExitedRegion, -- Bindable Event named PlayerExitedRegion
-	size = Vector3.new(1,1,1),
-	cFrame = nil
+	Size = Vector3.new(1,1,1),
+	Transform = nil
 }
 
--- | Private Functions
-local function filterPlayersInRegion(partsInRegion) -- Returns a table of all the players in a given region
-	local temp_table = {}
+-- // Private functions \\
+
+--- Returns a table of all the players in a given region
+---@param partsInRegion any
+local function filterPlayersInRegion(partsInRegion)
+	local tempTable = {};
+
 	for _,v in pairs(partsInRegion) do
-		if game:GetService("Players"):GetPlayerFromCharacter(v.Parent) and not table.find(temp_table, game:GetService("Players"):GetPlayerFromCharacter(v.Parent)) then
-			table.insert(temp_table, game:GetService("Players"):GetPlayerFromCharacter(v.Parent))
+		if game:GetService("Players"):GetPlayerFromCharacter(v.Parent) and not table.find(tempTable, game:GetService("Players"):GetPlayerFromCharacter(v.Parent)) then
+			table.insert(tempTable, game:GetService("Players"):GetPlayerFromCharacter(v.Parent));
 		end
 	end
-	return temp_table
+
+	return tempTable;
 end
 
-local function checkPlayersAdded(filteredTable) -- Checks if a new player was added to a table of players
+--- Checks if the new player was added to the table of players
+--- @param filteredTable any
+local function checkPlayersAdded(filteredTable)
 	for _,v in pairs(Region.PlayersInRegion) do
 		if not table.find(filteredTable, v) then
-			Region.PlayerExitedRegion:Fire(v)
-			table.remove(Region.PlayersInRegion, table.find(Region.PlayersInRegion, v))
+			Region.PlayerExitedRegion:Fire(v);
+			table.remove(Region.PlayersInRegion, table.find(Region.PlayersInRegion, v));
 		end
 	end
 end
 
-local function checkPlayersRemoved(filteredTable) -- Checks if a player was removed from a table of players
+--- Checks if a player was removed from a table of players
+--- @param filteredTable any
+local function checkPlayersRemoved(filteredTable)
 	for _,v in pairs(filteredTable) do
 		if not table.find(Region.PlayersInRegion, v) then
-			Region.PlayerEnteredRegion:Fire(v)
-			table.insert(Region.PlayersInRegion, v)
+			Region.PlayerEnteredRegion:Fire(v);
+			table.insert(Region.PlayersInRegion, v);
 		end
 	end
 end
 
--- | Public Functions
-function Region.new()
-	return require(script:Clone())
+--- Create a new BindableEvent object
+---@param name any
+---@param parent any
+local function createEvent(name, parent)
+	local object = Instance.new("BindableEvent");
+	object.Name = name;
+	object.Parent = parent;
 end
 
-function Region:CreateRegionFromBSP(BSP: BasePart, DestroyBSP: boolean) -- Creates region data based on BSP object data and destroys it given a passed argument
-	Region.size = BSP.Size
-	Region.cFrame = BSP.CFrame
+-- // Class constructor \\
+function Region.new()
+	local clone = script:Clone();
+
+	-- Initialize new objects if necessary
+	if clone["PlayerEnteredRegion"] == nil then
+		createEvent("PlayerEnteredRegion", clone);
+	end
+
+	if clone["PlayerExitedRegion"] == nil then
+		createEvent("PlayerExitedRegion", clone);
+	end
+
+	return require(clone);
+end
+
+-- // Public functions \\
+
+--- Creates region data based on a BSP object
+---@param BSP any
+function Region:CreateRegionFromBSP(BSP: BasePart, DestroyBSP: boolean) -- 
+	Region.Size = BSP.Size;
+	Region.Transform = BSP.Transform;
+
 	if DestroyBSP or DestroyBSP == nil then
-		BSP:Destroy()
+		BSP:Destroy();
 	end
 end
 
--- | Main
-game:GetService("RunService").Heartbeat:Connect(function() -- Checks if a player has entered a region every tick
+-- // Frame tick \\
+game:GetService("RunService").Heartbeat:Connect(function()
 	if Region.CheckPlayerEntry then
-		local filteredTable = filterPlayersInRegion(workspace:GetPartBoundsInBox(Region.cFrame, Region.size))
+		local filteredTable = filterPlayersInRegion(workspace:GetPartBoundsInBox(Region.Transform, Region.Size))
 		if filteredTable ~= Region.PlayersInRegion then
-			checkPlayersAdded(filteredTable)
-			checkPlayersRemoved(filteredTable)
+			checkPlayersAdded(filteredTable);
+			checkPlayersRemoved(filteredTable);
 		end
 	end
 end)
 
-return Region
+return Region;
+
+--// EOF \\
